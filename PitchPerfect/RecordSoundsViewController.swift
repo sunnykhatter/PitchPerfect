47238e54d408c675a01470473da65cb24be2bdc5
Lakshay Khatter <lakshaykhatter@gmail.com>
2016-08-30 10:14:31 -0400

    Imported AVfoundation and implemented AVAudioRecorder in the record and stop record audio functions

--- PitchPerfect/RecordSoundsViewController.swift
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -7,14 +7,17 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class RecordSoundsViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
     
     @IBOutlet weak var recordButton: UIButton!
     
     @IBOutlet weak var stopRecordingButton: UIButton!
+    
+    var audioRecorder: AVAudioRecorder!
     
     
     override func viewDidLoad() {
@@ -37,6 +40,22 @@ class ViewController: UIViewController {
         stopRecordingButton.enabled = true
         recordButton.enabled = false
         
+        let dirPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory,.UserDomainMask, true)[0] as String
+        let recordingName = "recordedVoice.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = NSURL.fileURLWithPathComponents(pathArray)
+        print(filePath)
+        
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+        
+        try! audioRecorder = AVAudioRecorder(URL: filePath!, settings: [:])
+        audioRecorder.meteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
+        let audioSession = AVAudioSession.sharedInstance()
+        try! audioSession.setActive(false)
+        
     }
     
     
@@ -44,6 +63,9 @@ class ViewController: UIViewController {
         recordingLabel.text = "Recording Stopped"
         stopRecordingButton.enabled = false
         recordButton.enabled = true
+        
+        audioRecorder.stop()
+        
         
     }
     
