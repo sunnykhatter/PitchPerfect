306ae5a949b6a6c76a220193084f04918fd13b9c
Lakshay Khatter <lakshaykhatter@gmail.com>
2016-08-29 21:37:40 -0400

    Created the logic for button states and embeded view into nagivation view controller

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -12,6 +12,11 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
     
+    @IBOutlet weak var recordButton: UIButton!
+    
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,15 +26,24 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        stopRecordingButton.enabled = false
+    }
 
 
     @IBAction func recordAudio(sender: AnyObject) {
         recordingLabel.text = "Recording in Progress"
+        stopRecordingButton.enabled = true
+        recordButton.enabled = false
+        
     }
     
     
     @IBAction func stopRecording(sender: AnyObject) {
         recordingLabel.text = "Recording Stopped"
+        stopRecordingButton.enabled = false
+        recordButton.enabled = true
         
     }
     
